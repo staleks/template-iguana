0126ccf1e1fc5d5b8cb0276aefbf4f8b062c01ec
astoisavljevic <astoisavljevic@jatheon.com>
2025-05-26 12:46:42 +0200

    fix failed compile

--- test-data/src/main/java/rs/in/staleks/template/iguana/testdata/FakerUserData.java
+++ test-data/src/main/java/rs/in/staleks/template/iguana/testdata/FakerUserData.java
@@ -1,13 +1,15 @@
 package rs.in.staleks.template.iguana.testdata;
 
 import com.github.javafaker.Faker;
+import rs.in.staleks.template.iguana.domain.management.Account;
 import rs.in.staleks.template.iguana.domain.management.User;
 
 public class FakerUserData {
 
     public static User randomUser() {
         Faker faker = new Faker();
-        return User.of(faker.number().randomNumber(), faker.name().username(), faker.internet().emailAddress());
+        Account testAccount = Account.create("Dummy Name");
+        return User.of(faker.number().randomNumber(), faker.name().username(), faker.internet().emailAddress(), testAccount);
     }
 
 }
